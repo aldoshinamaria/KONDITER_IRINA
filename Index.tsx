245ba5e2ab367c173b4bf98db33c65b3b775ac14
aldoshinamaria <aldosinamaria95@gmail.com>
2026-04-26 15:44:18 +0300

    Add files via upload

---
+++ Index.tsx
@@ -0,0 +1,27 @@
+import Header from "@/components/Header";
+import HeroSection from "@/components/HeroSection";
+import AboutSection from "@/components/AboutSection";
+import GallerySection from "@/components/GallerySection";
+import TestimonialsSection from "@/components/TestimonialsSection";
+import PriceSection from "@/components/PriceSection";
+import OrderSection from "@/components/OrderSection";
+import ContactSection from "@/components/ContactSection";
+import Footer from "@/components/Footer";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen">
+      <Header />
+      <HeroSection />
+      <AboutSection />
+      <GallerySection />
+      <PriceSection />
+      <TestimonialsSection />
+      <OrderSection />
+      <ContactSection />
+      <Footer />
+    </div>
+  );
+};
+
+export default Index;
